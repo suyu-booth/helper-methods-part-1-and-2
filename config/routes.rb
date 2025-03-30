af6afcf6ebc21fccae98ab7f85361a4271725821
suyu-booth <suyu@chicagobooth.edu>
2025-03-30 22:58:11 +0000

    6

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,19 @@ Rails.application.routes.draw do
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create", as: :movies 
+  post "/movies" => "movies#create", as: :movies
   get "/movies/new" => "movies#new", as: :new_movie
 
   # READ
   get "/movies" => "movies#index"
-  get "/movies/:id" => "movies#show", as: :details
+  get "/movies/:id" => "movies#show", as: :movie
 
   # UPDATE
   patch "/movies/:id" => "movies#update"
   get "/movies/:id/edit" => "movies#edit", as: :edit_movie
 
   # DELETE
-  delete "/movies/:id", controller: "movies", action: "destroy" 
+  delete "/movies/:id" => "movies#destroy"
 
   #------------------------------
 end
